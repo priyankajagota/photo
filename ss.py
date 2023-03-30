a10548360b06e5678baae8870ef897faf8cc8a44
priyankajagota <60633744+priyankajagota@users.noreply.github.com>
2023-03-30 00:14:29 -0400

    Update ss.py

--- ss.py
+++ ss.py
@@ -46,7 +46,7 @@ def page1():
         ### Every picture says some story, lets generate yours....
         """
     )
-    file = st.file_uploader('Please upload an image file', type = ['jpg','png','jpeg'])
+    file = st.file_uploader('Please upload an image file', type = ['jpg','png'])
     if file is None:
       st.write("")
     else:
